710bf776e3a6991190b307a2ac77af980185b30f
Kryštof Janků <krysot.janku@seznam.cz>
2026-08-11 21:45:17 +0800

    Prepare POS for Android with Capacitor

---
+++ android/app/src/main/java/cz/bigjohns/pos/MainActivity.java
@@ -0,0 +1,5 @@
+package cz.bigjohns.pos;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "cz.bigjohns.pos",
+  appName: "Big John's POS",
+  webDir: "dist",
+  android: {
+    backgroundColor: "#171b21",
+  },
+};
+
+export default config;
